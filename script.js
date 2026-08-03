02eb7e3e39ccc925445b1ab7badbe735331ebd1d
elnarkvees <elvisjuma416@gmail.com>
2026-08-03 12:52:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ function closeMenu(){
   document.getElementById("sideMenu")
   .classList.remove("active");
 }
+
+
+/* SCROLL ANIMATION */
+
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach((entry)=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+const hiddenElements=document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((el)=>observer.observe(el));
