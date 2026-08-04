459ffde1adf49fde687a6ab69ee1b9264e5d5323
elnarkvees <elvisjuma416@gmail.com>
2026-08-04 12:27:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -96,3 +96,38 @@ lightbox.addEventListener("click", (e) => {
   }
 
 });
+
+// ==========================
+// GALLERY FILTER
+// ==========================
+
+const filterButtons = document.querySelectorAll(".filter-btn");
+const galleryItems = document.querySelectorAll(".gallery-item");
+
+filterButtons.forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+filterButtons.forEach(btn=>btn.classList.remove("active"));
+
+button.classList.add("active");
+
+const filter = button.dataset.filter;
+
+galleryItems.forEach(item=>{
+
+if(filter==="all" || item.classList.contains(filter)){
+
+item.style.display="block";
+
+}else{
+
+item.style.display="none";
+
+}
+
+});
+
+});
+
+});
