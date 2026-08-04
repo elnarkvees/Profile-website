b7b90092e36a64fe047e6fe267beefdc99b9af52
elnarkvees <elvisjuma416@gmail.com>
2026-08-04 16:02:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -166,3 +166,35 @@ filterButtons.forEach(button => {
   });
 
 });
+
+// ==========================
+// BACK TO TOP
+// ==========================
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+  if(window.scrollY > 500){
+
+    backToTop.classList.add("show");
+
+  }else{
+
+    backToTop.classList.remove("show");
+
+  }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+  window.scrollTo({
+
+    top:0,
+
+    behavior:"smooth"
+
+  });
+
+});
