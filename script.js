507e9ecf56ac724c748d8945c0d59514329773cd
elnarkvees <elvisjuma416@gmail.com>
2026-08-05 11:52:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -133,41 +133,6 @@ item.style.display="none";
 });
 
 // ==========================
-// GALLERY FILTER
-// ==========================
-
-const filterButtons = document.querySelectorAll(".filter-btn");
-const galleryItems = document.querySelectorAll(".gallery-item");
-
-filterButtons.forEach(button => {
-
-  button.addEventListener("click", () => {
-
-    filterButtons.forEach(btn => btn.classList.remove("active"));
-
-    button.classList.add("active");
-
-    const filter = button.dataset.filter;
-
-    galleryItems.forEach(item => {
-
-      if(filter === "all" || item.classList.contains(filter)){
-
-        item.style.display = "block";
-
-      }else{
-
-        item.style.display = "none";
-
-      }
-
-    });
-
-  });
-
-});
-
-// ==========================
 // BACK TO TOP
 // ==========================
 
