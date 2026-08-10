6a9aa3841a81b7a20ede3d9831d60613cd65ace6
elnarkvees <elvisjuma416@gmail.com>
2026-08-10 23:48:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -29,36 +29,6 @@ const hiddenElements=document.querySelectorAll(".hidden");
 
 hiddenElements.forEach((el)=>observer.observe(el));
 
-/* ACTIVE NAVIGATION */
-
-const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll(".nav-link");
-
-window.addEventListener("scroll", ()=>{
-
-let current="";
-
-sections.forEach(section=>{
-
-const sectionTop = section.offsetTop-150;
-
-if(pageYOffset >= sectionTop){
-current = section.getAttribute("id");
-}
-
-});
-
-navLinks.forEach(link=>{
-
-link.classList.remove("active");
-
-if(link.getAttribute("href")==="#"+current){
-link.classList.add("active");
-}
-
-});
-
-});
 
 // ==========================
 // LIGHTBOX
